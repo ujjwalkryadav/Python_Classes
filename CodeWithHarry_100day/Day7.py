3874d04c7f56f1f267c0be2a3986058b6575a2f1
Ujjwal Yadav <ujjwalkryadav01@gmail.com>
2025-10-11 19:34:41 +0530

    Update And add My Learning

---
+++ CodeWithHarry_100day/Day7.py
@@ -0,0 +1,21 @@
+# Exercise 1 - Create a Calculator
+# Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers. Your program should format the output in a readable manner!
+
+
+#program Calculator -------------------
+a = 5
+b = 10
+print("First Number is = 5")
+print("second Number is = 10\n")
+
+print("Addition Of 5 and 10 =", a + b)
+print("Subtraction Of  5 and 10=", a - b)
+print("Multiplication Of 5 and 10=", a * b)
+print("Division Of 5 and 10=", a / b)
+print("floor division Of 5 and 10=", a // b)
+print("modulus Of 5 and 10=", a % b)
+print("exponentiation Of 5 and 10=", a ** b)
+#program 1 -------------------
+
+
+
--- Shashantsir_Python_Classes/Python_Day3.py
+++ Shashantsir_Python_Classes/Python_Day3.py
@@ -4,6 +4,3 @@ Where, who, when, why
 Python Frameworks ( Django, Flask, NumPy, Pnadas)
 Explore what Variables in Python
 '''
-
-
-
