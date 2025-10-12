40a8efd7a5e224592534bc4f071f001980d9d4ab
Ujjwal Yadav <ujjwalkryadav01@gmail.com>
2025-10-12 22:22:41 +0530

    Update Learning

---
+++ CodeWithHarry_100day/Day11.py
@@ -0,0 +1,39 @@
+# Diffrent Methods To write String
+print("Ujjwal Yadav")
+print('Ujjwal Yadav')
+
+print(''' Hello My Name Is Ujjwal
+ I Am from purvi champaran bihar''')
+
+print(""" Hello My Name Is Ujjwal
+ I Am from purvi champaran bihar""")
+
+# print('I can't Play Criicket')    error because i use (')
+print("I can't Play Criicket")
+
+# print("My Name "Ujjwal Yadav" ")    error because i use ("")
+print('My Name "Ujjwal Yadav"')
+
+
+
+
+#program 1 (Print Cherecter) ---------------
+name = 'Ujjwal'
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+# print(name[6]) Error
+#program end ---------------
+
+#program 3 (Print Cherecter using for loop) ---------------
+addresh = (''' Hello My Name Is Ujjwal
+ I Am from purvi champaran bihar''')
+
+for character in addresh:
+    print(character)
+#program end ---------------
+
