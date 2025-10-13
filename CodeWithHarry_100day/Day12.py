c7d248507858e5a096e3c5fac675a3e1028e36f2
Ujjwal Yadav <ujjwalkryadav01@gmail.com>
2025-10-13 18:17:52 +0530

    Learning Update

---
+++ CodeWithHarry_100day/Day12.py
@@ -0,0 +1,27 @@
+# Harry Sir Code Refrance
+# fruit = "Mango"
+# len1 = len(fruit)
+# print("Mango is a", len1, "letter word.")
+
+name = "Ujjwal"
+a = len(name)
+print(a) 
+print(name[0:6]) 
+print(name[-3:-2])     
+
+# Quick Task
+nm = "harry"
+print(nm[-4:-2])  #I think Out Put Is 0 because both side we minus over 
+# but i wrong harry ->[ h , a, r, r, y ]
+#                     [ 0,  1, 2, 3, 4,]
+                 #    [ -5,-4,-3,-2,-1 ]       
+#so -4 place is place of a and - 2 is placce of r so output is ar.
+
+
+
+
+'''
+Learned that the len() function is used to count the total number of characters in a word or sentence.
+Understood that each letter in a string has an index number, starting from 0 on the left and negative numbers (-1, -2, …) from the right.
+Learned about string slicing — [start:end] includes the start index but excludes the end index.
+Discovered how to print specific parts or letters from a word using index numbers and slicing. '''
