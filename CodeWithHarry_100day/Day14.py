616647daf408e5123050e3d03d8b2488836eb4cb
Ujjwal Yadav <ujjwalkryadav01@gmail.com>
2025-10-15 23:33:18 +0530

    Learning Update... If Else

---
+++ CodeWithHarry_100day/Day14.py
@@ -0,0 +1,12 @@
+# Today I Learn and know about  If, Elif, Else Keywords..
+age = int(input ("Enter Your age"))
+print("Your Age Is =",age)
+
+if(age >= 18):
+    print("You Can Drive")
+
+else:
+    print("No! You Can't Drive")
+
+
+
