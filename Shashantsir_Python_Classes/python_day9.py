41a99ff8ecf7285da66d6085ba66832f1e40f5d4
Ujjwal Yadav <ujjwalkryadav01@gmail.com>
2025-10-16 23:56:34 +0530

    Learning Update...
    '''
    Day 9 Summary
    ✅ Today I practiced:
    - Bitwise AND, OR, and XOR operators.
    - Binary number representation and result checking.
    - How to read binary-to-decimal results.
    - Membership operators: "in" and "not in".
    
    💡 Main concept learned:
    Bitwise operators work on binary bits directly,
    while membership operators check for value existence in sequences.
    '''

---
+++ Shashantsir_Python_Classes/python_day9.py
@@ -0,0 +1,48 @@
+# 1️⃣ Bitwise AND Operator ---------------
+a = 10
+b = 4
+x = 5
+y = 3
+
+print(a & b)   # 0
+print(x & y)   # 1
+print(5 & 6)   # 4
+#program END ---------------
+
+
+#program 2 ( Bitwise OR Operator ) ---------------
+A = 5
+B = 6
+print(A | B)
+#program END ---------------
+
+
+#program 3 ( Bitwise XOR Operator ) ---------------
+A = 5
+B = 6
+print(A ^ B)
+#program END ---------------
+
+
+#program 4 ( Membership Operators ) ---------------
+
+Ravi = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(5 in Ravi)       # True
+print(15 not in Ravi)  # True
+print(2 not in Ravi)   # False
+print(78 in Ravi)      # False
+#program END ---------------
+
+
+'''
+Day 9 Summary
+✅ Today I practiced:
+- Bitwise AND, OR, and XOR operators.
+- Binary number representation and result checking.
+- How to read binary-to-decimal results.
+- Membership operators: "in" and "not in".
+
+💡 Main concept learned:
+Bitwise operators work on binary bits directly, 
+while membership operators check for value existence in sequences.
+'''
