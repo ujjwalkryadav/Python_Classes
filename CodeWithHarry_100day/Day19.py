2ddd89681752e2e9eabb0837e011a9a0686146de
Ujjwal Yadav <ujjwalkryadav01@gmail.com>
2025-11-21 00:50:32 +0530

    Learning Update

---
+++ CodeWithHarry_100day/Day19.py
@@ -0,0 +1,25 @@
+#Today I learn break and continue statement.
+
+# # for i in range(100):
+# #     print(i)
+# #     if(i == 50):
+# #      break        # this stattement exit from loop if condition if false
+
+
+# for i in range(2 ,100):
+#    if(i % 2 == 0):
+#       continue          #use of condition we skip even number
+#    print(i)
+
+
+
+# task:-  DO while loop not in python but we can create by logocaly so create that
+
+i = 1
+while True:
+    print(i)
+    i = i+1
+    if(i == 5):
+        break
+# -------------------------------
+
