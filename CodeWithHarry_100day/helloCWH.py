32171b8acd457219235848864a0c69f6f7a9f49e
Ujjwal Yadav <ujjwalkryadav01@gmail.com>
2025-11-21 00:50:45 +0530

    Add Day19.py to demonstrate break and continue statements

--- CodeWithHarry_100day/helloCWH.py
+++ CodeWithHarry_100day/helloCWH.py
@@ -1,1 +1,11 @@
-print("Jai_sri_ram")
+# print("Jai_sri_ram")
+num = int(input("Enter a number: "))
+match num:
+    case 0:
+        print("Number is zero")
+    case _:
+        match num % 2:
+            case 0:
+                print(num, "is Even")
+            case 1:
+                print(num, "is Odd")
---
+++ CodeWithHarry_100day/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+for i in range(100):
+#     print(i)
+#     if(i == 50):
+#      break        # this stattement exit from loop if condition if false
